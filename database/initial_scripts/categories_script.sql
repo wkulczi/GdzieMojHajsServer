fb28ddca824e3510044e7571a3b6346fe15a4b74
wkulczi <wojciechkulczak@gmail.com>
2020-06-05 17:22:27 +0200

    modified categories_script.sql

--- database/initial_scripts/categories_script.sql
+++ database/initial_scripts/categories_script.sql
@@ -1,9 +1,10 @@
-INSERT INTO category (category_name, description)
-VALUES ( 'Jedzenie', 'W tej kategorii znajdują się dane dotyczace środków wydanych na jedzenie.'),
-    ( 'Relaks',  'W tej kategorii znajdują się dane dotyczace środków wydanych na relaks.'),
-    ( 'Odzież',  'W tej kategorii znajdują się dane dotyczace środków wydanych na odziez.'),
-    ( 'Sprzęt',  'W tej kategorii znajdują się dane dotyczace środków wydanych na sprzęt.'),
-    ( 'Mieszkanie',  'W tej kategorii znajdują się dane dotyczace środków wydanych na mieszkanie.'),
-    ( 'Transport',  'W tej kategorii znajdują się dane dotyczace środków wydanych na transport.'),
-    ( 'Higiena', 'W tej kategorii znajdują się dane dotyczace środków wydanych na środki higieniczne.'),
-    ( 'Rachunki',  'W tej kategorii znajdują się dane dotyczace środków wydanych na rachunki.');
+INSERT INTO category (category_name,category_name_eng, description)
+VALUES ( 'Jedzenie', 'Food','W tej kategorii znajdują się dane dotyczace środków wydanych na jedzenie.'),
+    ( 'Relaks', 'Relax', 'W tej kategorii znajdują się dane dotyczace środków wydanych na relaks.'),
+    ( 'Odzież', 'Clothes', 'W tej kategorii znajdują się dane dotyczace środków wydanych na odziez.'),
+    ( 'Sprzęt', 'Equipment', 'W tej kategorii znajdują się dane dotyczace środków wydanych na sprzęt.'),
+    ( 'Mieszkanie','House',  'W tej kategorii znajdują się dane dotyczace środków wydanych na mieszkanie.'),
+    ( 'Transport', 'Transport', 'W tej kategorii znajdują się dane dotyczace środków wydanych na transport.'),
+    ( 'Higiena', 'Hygiene','W tej kategorii znajdują się dane dotyczace środków wydanych na środki higieniczne.'),
+    ( 'Rachunki', 'Taxes', 'W tej kategorii znajdują się dane dotyczace środków wydanych na rachunki.'),
+    ( 'Inne', 'Other', ' W tej kategorii znajdują się dane dotyczace środków wydanych na inne wydatki, niezawarte w dostępnych kategoriach.');
